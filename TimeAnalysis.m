159c96aba2c13de04c9bd2ef1dbb86f4dd9c1d7f
Aaron Meyer <aminute@gmail.com>
2015-12-06 19:23:20 -0500

    More integration.

--- TimeAnalysis.m
+++ TimeAnalysis.m
@@ -149,3 +149,88 @@ end
 function out = rowFilter(dataM, string)
     out = dataM(not(cellfun('isempty', strfind(dataM.RowNames, string))),:);
 end
+
+
+function [T, data, mmean, sem, auc] = getDataInt(GF, Plan, CellLine, Site, pplot, ccolor)
+
+if nargin < 5
+    pplot = 0;
+end
+
+if nargin < 6
+    ccolor = 'k';
+end
+
+[num, txt] = xlsread('FullSet.xls');
+
+colIDX = find(strcmp(txt(1,:), Site)) - 3;
+
+GFIDX = find(strcmp(txt(:,3),GF));
+PlanIDX = find(strcmp(txt(:,2),Plan));
+CellIDX = find(strcmp(txt(:,1),CellLine));
+
+rowIDX = intersect(intersect(GFIDX, PlanIDX), CellIDX)-1;
+
+
+data = num(rowIDX, colIDX);
+
+
+T = num(rowIDX, 1);
+mmean = mean(data, 2);
+sem = std(data, 0, 2) ./ sqrt(size(data, 2));
+
+mmeanUp = mmean + sem;
+mmeanDown = mmean - sem;
+
+
+for ii = 1:size(data,2)
+    auc(ii) = trapz(T, data(:,ii)-data(1,ii));
+end
+
+if pplot
+    h = fill( [T; flipud(T)],  [mmeanUp; flipud(mmeanDown)], ccolor);
+    h.LineWidth = 0.01;
+    hold on;
+    alpha(0.25);
+    plot(T, mmean, ccolor, 'LineWidth', 2);
+    axis([min(T) max(T) 0 max(mmeanUp)*1.1]);
+    ylabel('FI')
+    xlabel('Time (min)');
+end
+
+end
+
+
+
+function DM = getDataMat
+
+import bioma.data.*;
+
+[num, txt] = xlsread('FullSet.xls');
+
+for ii = 2:size(txt,1)
+    txtRow{ii-1} = [txt{ii,1} '-' txt{ii,2} '-' txt{ii,3}]; %#ok<AGROW>
+end
+
+DM = DataMatrix(num,txtRow,txt(1,4:end));
+
+sites = {'AKT', 'MEK', 'JNK', 'ERK', 'cJUN', 'STAT3', 'GSK'};
+
+for ii = 1:length(sites)
+    neww = DataMatrix([nanmean(double(DM(:,sites{ii})),2), ...
+        nanstd(double(DM(:,sites{ii})),0,2)/sqrt(3)], ...
+        txtRow,{[sites{ii} 'm'],[sites{ii} 's']});
+    DM(:,sites{ii}) = [];
+    DM = horzcat(DM, neww); %#ok<AGROW>
+end
+
+end
+
+
+
+
+
+
+
+
+
